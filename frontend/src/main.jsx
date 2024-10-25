3010ffd635b1d7ef9c059cc4872fdb990390b25a
Ajith Emmanuel A <ajithemmanuel10@gmail.com>
2024-10-25 21:09:39 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,5 +11,5 @@ import 'sweetalert2/dist/sweetalert2.js'
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <RouterProvider router={router} />
-  </Provider >,
+  </Provider >, 
 )
